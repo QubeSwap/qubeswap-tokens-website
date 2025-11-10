67d68a02e071c4bcc54d5f7cd7cba73085c86724
ROD ZISSO <muroko@users.noreply.github.com>
2025-11-10 10:15:43 -0500

    Remove social media links from footer
    Removed Twitter and GitHub links from the footer.

--- XQST/components/Footer/index.tsx
+++ XQST/components/Footer/index.tsx
@@ -8,12 +8,7 @@ export const Footer = () => {
     <div className="flex flex-col gap-2 p-4">
       <div className='flex items-center justify-center gap-6'>
         <hr className='w-10 bg-white' />
-        <CustomLink href="https://x.com/QubeSwap">
-          <BsTwitter color='white' />
-        </CustomLink>
-        <CustomLink href="https://github.com/QubeSwap">
-          <BsGithub color='white' />
-        </CustomLink>
+        
         <hr className='w-10 bg-white' />
       </div>
     </div>
