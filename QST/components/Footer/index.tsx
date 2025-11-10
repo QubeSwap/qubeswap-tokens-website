c4ce3dd93b1cc04eb31ffe86ed8a004089bfe1b5
MUROKO <muroko@users.noreply.github.com>
2025-11-10 09:42:59 -0500

    update: QST website 2

--- QST/components/Footer/index.tsx
+++ QST/components/Footer/index.tsx
@@ -1,5 +1,6 @@
 
 import { BsTwitter, BsGithub } from "react-icons/bs"
+import { FaTelegramPlane } from "react-icons/fa";
 import { CustomLink } from "../CustomLink"
 
 export const Footer = () => {
@@ -7,10 +8,13 @@ export const Footer = () => {
     <div className="flex flex-col gap-2 p-4">
       <div className='flex items-center justify-center gap-6'>
         <hr className='w-10 bg-white' />
-        <CustomLink href="https://twitter.com/intent/user?screen_name=albertocruzdev">
+        <CustomLink href="https://x.com/QubeSwap">
           <BsTwitter color='white' />
         </CustomLink>
-        <CustomLink href="https://github.com/AlbertoCruzLuis">
+		<CustomLink href="https://t.me/QubeSwap">
+          <FaTelegramPlane color='white' />
+        </CustomLink>
+        <CustomLink href="https://github.com/QubeSwap">
           <BsGithub color='white' />
         </CustomLink>
         <hr className='w-10 bg-white' />
--- QST/content/TokenomicsSection/index.tsx
+++ QST/content/TokenomicsSection/index.tsx
@@ -6,17 +6,21 @@ export const TokenomicsSection = () => {
       <h2 className="pt-8 pb-4 text-3xl font-bold text-center text-white">Tokenomics</h2>
       <div className="flex justify-center gap-12 px-4 py-8 xs:flex-col">
         <ToolTipPieChart data={[
-          { title: "Airdrop", value: 2, color: "#E38627" },
-          { title: "Owner", value: 98, color: "#C13C37" }
+          { title: "Qubetars NFT Rewards Reserve (25M)", value: 25, color: "#C13C37" },
+          { title: "QubePlay Rewards Reserve (5M)", value: 5, color: "#471614" },
+		  { title: "Trading Liquidity Reserve (10M)", value: 10, color: "#d26560" },
+		  { title: "QST Presale Reserve (10M)", value: 10, color: "#563a18" },
+		  { title: "QubeApp Staking Reserve (10M)", value: 10, color: "#dbb17e" },
+		  { title: "XQST Initial Supply Reserve (40M)", value: 40, color: "#E38627" }
         ]} />
         <div className="grid grid-cols-1 p-2 xs:grid-cols-2 place-items-center">
           <div className="flex flex-col gap-2">
-            <span className="text-lg text-white">$FXC</span>
             <span className="text-xs text-neutral-300">Token Symbol</span>
+			<span className="text-lg text-white">$QST</span>
           </div>
           <div className="flex flex-col gap-2">
-            <span className="text-lg text-white">5.000</span>
-            <span className="text-xs text-neutral-300">Total Supply</span>
+            <span className="text-xs text-neutral-300">Max Supply</span>
+			<span className="text-lg text-white">100M</span>
           </div>
         </div>
       </div>
